6073b04fb52f4e25030c9a62eca7c247e5a6fb1f
zhengdehao <2409252135@qq.com>
2020-12-22 21:21:47 +0800

    zhengdehao v1.1.5

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const detailModel = require('../model/detail.js');
+const detailModel = require('../model/recommend.js');
 
-router.post('/detail', (req, res, next) => {
+router.post('/recommend', (req, res, next) => {
   // 第四步， 查询， 根据模型进行查询
-  // detailModel.find(function (err, kittens) {
-  //   if (err) return console.error(err);
-  //   console.log(kittens);
-  // })
   console.log(req.body.id)
   detailModel.find({ cityId: Number(req.body.id) }, (err, doc) => {
     console.log(doc);
@@ -16,6 +12,19 @@ router.post('/detail', (req, res, next) => {
       status: 0,
       result: doc
     })
+  })
+}) 
+
+router.get('/banner', (req, res, next) => {
+  res.json({
+    status: 0,
+    result: {
+      list: [
+        "//gw.alicdn.com/imgextra/i2/48/O1CN01cjcmAm1CE1eo0mivo_!!48-0-lubanu.jpg",
+        "//img.alicdn.com/imgextra/i1/127/O1CN01qQyzyw1CoCwhyH89F_!!127-0-luban.jpg",
+        "//img.alicdn.com/imgextra/i2/143/O1CN010jC8291CvXHYvkVeH_!!143-0-luban.jpg"
+      ]
+    }
   })
 })
 
